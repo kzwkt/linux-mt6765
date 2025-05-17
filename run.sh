8373e813e9b97d775384cca87ac6d97e57c41036
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-17 19:23:44 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set environment variables
+#git clone --depth=1 https://github.com/picasso09/proton-clang clang
+#mkdir clang && cd clang && wget https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r522817.tar.gz && tar xf clang-r522817.tar.gz && rm clang-r522817.tar.gz && cd ..
+export KBUILD_BUILD_HOST="kitty"
+export KBUILD_BUILD_USER="meow"
+export PATH="$(pwd)/clang/bin:$PATH"
+
+# build kernel
+make -j$(nproc --all) O=out ARCH=arm64 defconfig
+make -j$(nproc --all) ARCH=arm64 O=out \
+                      HOSTCC="ccache clang" \
+                      HOSTCXX="ccache clang++" \
+                      CC="ccache clang" \
+                      LD=ld.lld \
+                      AR=llvm-ar \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      READELF=llvm-readelf \
+                      OBJSIZE=llvm-size \
+                      STRIP=llvm-strip \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
