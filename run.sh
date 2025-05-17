19f5655559bfab29d11a9fbcbeaa0071e3268f32
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-05-17 19:28:05 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,13 +13,5 @@ make -j$(nproc --all) ARCH=arm64 O=out \
                       HOSTCC="ccache clang" \
                       HOSTCXX="ccache clang++" \
                       CC="ccache clang" \
-                      LD=ld.lld \
-                      AR=llvm-ar \
-                      NM=llvm-nm \
-                      OBJCOPY=llvm-objcopy \
-                      OBJDUMP=llvm-objdump \
-                      READELF=llvm-readelf \
-                      OBJSIZE=llvm-size \
-                      STRIP=llvm-strip \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
